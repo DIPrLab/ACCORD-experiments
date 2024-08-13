bf868bda0238da850d2df1eaaf85a5e08e285774
Grace Hunter <39381292+Grace-H@users.noreply.github.com>
2024-08-12 23:09:32 -0700

    edit and update permissions actions

--- scripts/expr_1.py
+++ scripts/expr_1.py
@@ -3,29 +3,74 @@
 #   - Users: 5
 #   - Conflicts: 15
 #   - Actions: 100
+#   - Documents: 5 (each user initialized with one)
+#   - Actions allowed: Edit, Add Permission, Remove Permission, and Update Permission
+#   - Actions disallowed (to keep documents constant): Delete, Remove, Move
 
 import random
+import yaml
 
 from scripts.google_api_util import UserSubject, Resource
 
+all_roles = set(['owner', 'writer', 'commenter', 'reader'])
+
+# Initialize users
+with open('scripts/.user_info', 'r') as file:
+    user_info = yaml.safe_load(file)
+users = list(map(lambda u: UserSubject(u['name'], u['email'], u['token'],), user_info))
+users_by_id = {u.id : u for u in users}
+
 # Perform 100 random actions
-user_info = [("tokens/token_alice.json", "Alice"),
-        ("tokens/token_bob.json", "Bob"),
-        ("tokens/token_carol.json", "Carol"),
-        ("tokens/token_drew.json", "Drew"),
-        ("tokens/token_emily.json", "Emily"), ]
+for i in range(100):
+    if i % 10 == 0:
+        print(i)
 
-users = list(map(lambda u: UserSubject(u[0], u[1]), user_info))
+    # First randomly generated action may not be possible
+    action_completed = False
+    while not action_completed:
 
-for _ in range(100):
-    # Choose user & target resource
-    user = random.choice(users)
-    resources = user.list_resources()
-    target_res = None
+        # Choose user & target resource
+        user = random.choice(users)
+        resources = user.list_resources()
+        target_res = None
 
-    # Choose action to simulate
-    actions = ["Create"]
-    if resources:
-        target_res = random.choice(resources)
-        actions += user.file_actions(target_res)
-    action = random.choice(actions)
+        # Choose action to simulate
+        actions = []
+        if resources:
+            target_res = random.choice(resources)
+            actions += user.file_actions(target_res)
+        if "Delete" in actions:
+            actions.remove("Delete")
+        if "Remove" in actions:
+            actions.remove("Remove")
+        if "Move" in actions:
+            actions.remove("Move")
+
+        if not actions:
+            continue # Attempt with another user and document if no actions
+        action = random.choice(actions)
+
+        print(action)
+        if action == "Edit":
+            user.edit(target_res)
+        elif action == "AddPermission":
+            pass
+        elif action == "RemovePermission":
+            pass
+        elif action == "UpdatePermission":
+            permissions = user.list_permissions(target_res)
+            if len(permissions) < 2:
+                continue # Try another action if there's only one user with access to the resource
+
+            target_id = random.choice([u for u, v in permissions.items() if v != "owner"])
+            target_user = users_by_id[target_id]
+            current_role = permissions[target_id]
+
+            possible_roles = [role for role in all_roles if role != current_role]
+            if permissions[user.id] != "owner":
+                possible_roles.remove("owner")
+            new_role = random.choice(possible_roles)
+
+            user.update_permission(target_res, target_user, new_role)
+
+        action_completed = True
--- scripts/google_api_util.py
+++ scripts/google_api_util.py
@@ -1,37 +1,55 @@
 from collections import namedtuple
+from datetime import datetime
+from dataclasses import dataclass
+
+from googleapiclient.http import MediaInMemoryUpload
 
 from src.serviceAPI import create_user_driveAPI_service
 
-Resource = namedtuple("Resource", ["id", "name", "capabilities"])
+class ActionNotPermitted(Exception):
+    '''Exception raised for resource operations not permitted by file system state'''
+
+@dataclass
+class Resource():
+    id: str
+    name: str
+    capabilities: dict
+    mime_type: dict
 
 class UserSubject():
-    def __init__(self, token, name):
+    def __init__(self, name, email, token):
         '''
         
         Args:
             token: str, path to user's Drive token
         '''
         self.drive = create_user_driveAPI_service(token)
-        self.id = ""
+        self.id = self.drive.about().get(fields="user").execute()["user"]["permissionId"]
         self.name = name
+        self.email = name + "@accord.foundation"
 
     def list_resources(self):
         '''Retrieve a list of all files and folders a user has access to.
 
         Returns: list[DriveResource]'''
-        res = self.drive.files().list(fields="files(id, name, capabilities)", q="mimeType='application/vnd.google-apps.document' and trashed=false").execute()
-        files = list(map(lambda item: Resource(item["id"], item["name"], item["capabilities"]), res['files']))
-        res = self.drive.files().list(fields="files(id, name, capabilities)", q="mimeType='application/vnd.google-apps.folder' and trashed=false").execute()
-        folders = list(map(lambda item: Resource(item["id"], item["name"], item["capabilities"]), res['files']))
-        return files + folders
+        res_files = self.drive.files().list(fields="files(id, name, capabilities, mimeType)", q="mimeType='application/vnd.google-apps.document' and trashed=false").execute()
+        res_folders = self.drive.files().list(fields="files(id, name, capabilities, mimeType)", q="mimeType='application/vnd.google-apps.folder' and trashed=false").execute()
+        all_resources = res_files['files'] + res_folders['files']
+        parsed_resources = list(map(lambda item: Resource(item["id"], item["name"], item["capabilities"], item["mimeType"]), all_resources))
+        return parsed_resources
+
+    def list_permissions(self, resource):
+        '''Get permissions for all users on a resource'''
+        res = self.drive.permissions().list(fileId=resource.id).execute()['permissions']
+        permissions = {p['id'] : p['role'] for p in res}
+        return permissions
 
     def file_actions(self, resource):
-        '''Get a list of actions user is permitted on this resource'''
+        '''Get a list of actions user is permitted to take on this resource'''
         if not resource:
             return []
 
         capabilities = resource.capabilities
-
         actions = []
         if capabilities["canEdit"]:
             actions.append("Edit")
@@ -47,3 +65,42 @@ class UserSubject():
             actions.append("Remove")
 
         return actions
+
+    def edit(self, resource):
+        '''Edit a file or rename a folder'''
+        if not resource.capabilities["canEdit"]:
+            raise ActionNotPermitted("Edit not permitted on this resource.")
+
+        if resource.mime_type == 'application/vnd.google-apps.document':
+            # Add text to documents
+            file_content = "Hello World! File has been edited on " + str(datetime.now())
+            media = MediaInMemoryUpload(file_content.encode(), mimetype='application/vnd.google-apps.document')
+            self.drive.files().update(fileId=resource.id, media_body=media).execute()
+
+        elif resource.mime_type == 'application/vnd.google-apps.folder':
+            # Rename folders
+            new_name = resource.name.split(',')[0] + ',' + str(datetime.now())
+            self.drive.files().update(fileId=resource.id, body={"name": new_name}).execute()
+
+    def update_permission(self, resource, user, role):
+        '''Attempt to change another user's permission level on a resource.
+
+        Only checks that user has sharing permissions, not whether this user can transfer
+        ownership or whether change effectively does anything (e.g. changing editor to editor)
+
+        Args:
+            resource: resource to change permission on
+            user: UserSubject with pre-existing permission
+            role: new role for user
+        '''
+        if not resource.capabilities["canShare"]:
+            raise ActionNotPermitted("Insufficient permissions to change sharing")
+
+        new_permission = {'role' : role }
+        if role != "owner":
+            self.drive.permissions().update(fileId=resource.id, permissionId=user.id, body=new_permission).execute()
+        else:
+            self.drive.permissions().update(fileId=resource.id, permissionId=user.id, body=new_permission, transferOwnership=True).execute()
+
+    def __repr__(self):
+        return self.name
