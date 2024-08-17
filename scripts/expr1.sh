5ebb57ec3201e9fab2f63aa663e3bb363353c05c
Grace Hunter <39381292+Grace-H@users.noreply.github.com>
2024-08-16 17:37:59 -0700

    initial experiment 1--too many conflicts

---
+++ scripts/expr1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+for (( i = 0 ; i < 4; i++)); do
+    python3 scripts/expr1_log_generation.py
+done
--- scripts/expr1_detection.py
+++ scripts/expr1_detection.py
@@ -1,11 +1,198 @@
-import sys
-from csv import DictReader
+import sys, random, time
+from datetime import datetime, timezone
+from csv import DictReader, reader
 
-if len(sys.argv) < 2:
-    print("Usage: scripts/expr1_detection.py <log file>")
+from src.conflictDetctionAlgorithm import detectmain
 
-log_file = sys.argv[1]
+if len(sys.argv) < 3:
+    print("Usage: scripts/expr1_detection.py <num conflicts> <log file>")
+
+log_file = sys.argv[2]
 with open(log_file, "r") as csv_file:
     activities = list(DictReader(csv_file))
 
-print(activities)
+with open(log_file, "r") as csv_file:
+    logs = list(reader(csv_file))
+
+num_conflicts = int(sys.argv[1])
+total_constraints = 100
+
+# Useful constants
+constraint_names = {
+    "Can Edit": "Edit",
+    "Time Limit Edit": "Edit",
+    "Add Permission": "Permission Change",
+    "Remove Permission": "Permission Change",
+    "Update Permission": "Permission Change",
+}
+constraint_types = ["Time Limit Edit", "Add Permission", "Remove Permission", "Update Permission"]
+
+doc_ids = set()
+doc_names = {}
+actors = set()
+
+duplicate_constraints = [] # Constraints causing more than one conflict
+conflict_constraints = {} # two-level, first key is docID, second is actor
+# Generate 15 action constraints that cause conflicts
+for activity in activities:
+    doc_ids.add(activity['Doc_ID'])
+    doc_names[activity['Doc_ID']] = activity['Doc_Name']
+    actors.add(activity['Actor_Name'])
+
+    constraint = None
+    if activity['Action'] == "Edit":
+        constraint_type = random.choice(["Time Limit Edit"])
+        if constraint_type == "Time Limit Edit":
+            new_time = activity['Activity_Time'].split('.')
+            new_time = new_time[0] + ".000Z"
+            constraint = [
+                    activity['Doc_Name'],
+                    activity['Doc_ID'],
+                    "Edit",
+                    constraint_type,
+                    activity['Actor_Name'],
+                    "TRUE",
+                    "lt",
+                    None, # Owner email? Not used by engine
+                    new_time,
+            ]
+        else:
+            constraint = [
+                    activity['Doc_Name'],
+                    activity['Doc_ID'],
+                    "Edit",
+                    constraint_type,
+                    activity['Actor_Name'],
+                    "FALSE",
+                    "eq",
+                    None, # Owner email? Not used by engine
+                    "-",
+            ]
+    elif activity['Action'][0] == 'P': # Permission change
+        action_details = activity['Action'].split("-")
+        constraint_name = action_details[0]
+
+        new_permission = action_details[1].split(':')[1]
+        previous_permission = action_details[2].split(':')[1]
+        if new_permission == "none":
+            constraint_type = "Remove Permission"
+        elif previous_permission == "none":
+            constraint_type = "Add Permission"
+        else:
+            constraint_type = "Update Permission"
+
+        constraint = [
+            activity['Doc_Name'],
+            activity['Doc_ID'],
+            constraint_name,
+            constraint_type,
+            activity['Actor_Name'], # Email
+            "TRUE",
+            "eq",
+            None, # Owner email? Not used by engine
+            '-',
+        ]
+
+    if constraint[1] in conflict_constraints:
+        if (constraint[4] in conflict_constraints[constraint[1]] and 
+                constraint in conflict_constraints[constraint[1]][constraint[4]]):
+            conflict_constraints[constraint[1]][constraint[4]].remove(constraint)
+            duplicate_constraints.append(constraint)
+            continue
+    if constraint[1] not in conflict_constraints:
+        conflict_constraints[constraint[1]] = {}
+    if constraint[4] not in conflict_constraints[constraint[1]]:
+        conflict_constraints[constraint[1]][constraint[4]] = []
+
+    conflict_constraints[constraint[1]][constraint[4]].append(constraint)
+
+print(duplicate_constraints)
+chosen_constraints = random.sample([cons for key in conflict_constraints
+                                    for clist in conflict_constraints[key]
+                                    for cons in conflict_constraints[key][clist]], num_conflicts)
+
+constraints = {}
+for c in chosen_constraints:
+    if c[1] not in constraints:
+        constraints[c[1]] = {}
+    if c[4] not in constraints[c[1]]:
+        constraints[c[1]][c[4]] = []
+    constraints[c[1]][c[4]].append(c)
+
+doc_ids = list(doc_ids)
+actors = list(actors)
+
+# timelimit_stamp = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ")
+# remaining_constraints = total_constraints - num_conflicts
+# while remaining_constraints > 0:
+#     resource = random.choice(doc_ids)
+#     actor = random.choice(actors)
+#     constraint_type = random.choice(constraint_types)
+#     constraint_name = constraint_names[constraint_type]
+
+#     if constraint_type == "Edit":
+#         value = "FALSE"
+#     else:
+#         value = "TRUE"
+
+#     if constraint_type == "Time Limit Edit":
+#         comparator = "lt"
+#     else:
+#         comparator = "eq"
+
+#     if constraint_name == "Permission Change":
+#         true_value = random.choice(actors)
+#     elif constraint_type == "Time Limit Edit":
+#         true_value = timelimit_stamp
+#     else:
+#         true_value = "-"
+
+#     constraint = [
+#         doc_names[resource],
+#         resource,
+#         constraint_name,
+#         constraint_type,
+#         actor,
+#         value,
+#         comparator,
+#         None,
+#         true_value,
+#     ]
+
+#     # Duplicate
+#     if constraint[1] in constraints:
+#         if (constraint[4] in constraints[constraint[1]] and 
+#                 constraint in constraints[constraint[1]][constraint[4]]):
+#             continue
+#     # Causes conflict
+#     if constraint[1] in conflict_constraints:
+#         if constraint[4] in conflict_constraints[constraint[1]]:
+#             continue
+#     if constraint in duplicate_constraints:
+#         continue
+
+#     if constraint[1] not in constraints:
+#         constraints[constraint[1]] = {}
+#     if constraint[4] not in constraints[constraint[1]]:
+#         constraints[constraint[1]][constraint[4]] = []
+
+#     constraints[constraint[1]][constraint[4]].append(constraint)
+#     remaining_constraints -= 1
+
+formatted_constraints = {}
+total = 0
+for k, v in constraints.items():
+    formatted_constraints[k] = [cons for clist in v for cons in v[clist]]
+    total += len(formatted_constraints[k])
+
+logs = logs[1:]
+T0 = time.time()
+result = detectmain(logs, formatted_constraints)
+T1 = time.time()
+
+
+conflicts = sum(result)
+print(result, conflicts, num_conflicts)
+#assert conflicts == num_conflicts
+
+print(T1 - T0)
